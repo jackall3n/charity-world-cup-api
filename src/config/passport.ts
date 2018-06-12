50802a596710c5de5057191c2fc5a43fbd6338c9
Jack Allen <jackallensemail+git@gmail.com>
2018-06-12 15:27:15 +0100

    changes

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -5,7 +5,7 @@ import User from "../db/schemas/user";
 export default function (passport: any) {
     let options = {
         jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('jwt'),
-        secretOrKey: config.auth.secret
+        secretOrKey: config.auth.secret || "test"
     };
 
     passport.use(new Strategy(options, (payload, done) => {
